2af183f16f24785973758d45e373cb8cb753fee1
travismacdonald <x2015khn@stfx.ca>
2021-01-28 18:21:17 -0400

    added example unit tests using JUnit4

---
+++ project/src/main/java/com/gameofthreads/project/ExampleClass.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gameofthreads.project;
+
+/**
+ * TODO: this class can be deleted once we implement actual classes.
+ *       The only purpose of this class is for placeholder tests.
+ */
+public class ExampleClass {
+    
+    public String exampleVar = "some string";
+    
+    public ExampleClass() {
+        
+    }
+    
+    public int addTwoInts(int a, int b) {
+        return a + b;
+    }
+    
+    public boolean checkNumsAreSame(int a, int b) {
+        return a == b;
+    }
+    
+}
