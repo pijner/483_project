3c0f033ec489d8b28bb29927416c277fa7676e7a
Prahar Ijner <44989188+pijner@users.noreply.github.com>
2021-02-21 23:04:09 -0400

    UserBean test comments removed

--- project/src/main/java/com/gameofthreads/project/controller/UserBean.java
+++ project/src/main/java/com/gameofthreads/project/controller/UserBean.java
@@ -78,51 +78,6 @@ public class UserBean implements Serializable {
         
         try {
             this.model = dbc.getCompanyShifts(employee.getEmployeeCompany().getCompany_id(), start, end);
-            
-//        ArrayList<Shift> shifts = this.employee.getEmployeeShifts();
-//        this.model = new TimelineModel<>();
-//        
-//        for(Shift s: shifts){
-//            TimelineEvent e = TimelineEvent.builder()
-//                    .startDate(s.getStartTime())
-//                    .endDate(s.getEndTime())
-//                    .group(this.employee.getName())
-//                    .data(s.getShift_id())
-//                    .build();
-//            
-//            this.model.add(e);
-//            System.out.println("Shift " + s.getShift_id() + " added");
-//        }
-            
-//        start = LocalDate.of(-140, 1, 1).atStartOfDay();
-//        end = LocalDate.of(-140, 1, 2).atStartOfDay();
-//
-//        String[] NAMES = new String[] {"User 1", "User 2", "User 3", "User 4", "User 5", "User 6"};
-//                
-//        model = new TimelineModel<>();
-//        for (String name : NAMES) {
-//            LocalDateTime end = start.minusHours(12).withMinute(0).withSecond(0).withNano(0);
-//
-//            for (int i = 0; i < 5; i++) {
-//                LocalDateTime start = end.plusHours(Math.round(Math.random() *5));
-//                end = start.plusHours(4 + Math.round(Math.random() *5));
-//
-//                long r = Math.round(Math.random() * 2);
-//                String availability = (r == 0 ? "Unavailable" : (r == 1 ? "Available" : "Maybe"));
-//  
-//                // create an event with content, start / end dates, editable flag, group name and custom style class
-//                TimelineEvent event = TimelineEvent.builder()
-//                        .data(availability)
-//                        .startDate(start)
-//                        .endDate(end)
-//                        .editable(true)
-//                        .group(name)
-//                        .styleClass(availability.toLowerCase())
-//                        .build();
-//
-//                model.add(event);
-//            }  
-//        }  
         } catch (SQLException ex) {
             Logger.getLogger(UserBean.class.getName()).log(Level.SEVERE, null, ex);
         }
