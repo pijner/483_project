427a0211930114c2196761b566c4ddfe666afb11
travismacdonald <macdonaldtravisc@gmail.com>
2021-04-05 22:56:19 -0300

    Added some basic registration logic.
    Added registration validation cases. Regex for email address; email
    address stripping; password matching; already existing email.

--- project/src/main/java/com/gameofthreads/project/controller/RegisterBean.java
+++ project/src/main/java/com/gameofthreads/project/controller/RegisterBean.java
@@ -1,6 +1,8 @@
 package com.gameofthreads.project.controller;
 
 import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -13,9 +15,15 @@ import javax.inject.Named;
 @Named("registerBean")
 @SessionScoped
 public class RegisterBean implements Serializable {
+
     private String email;
     private String firstPassword;
     private String secondPassword;
+
+    private DBConnector dbConnector;
+
+    private boolean registerError = false;
+    private String errorMessage = "";
 
     public String getEmail() {
         return email;
@@ -40,18 +48,73 @@ public class RegisterBean implements Serializable {
     public void setSecondPassword(String secondPassword) {
         this.secondPassword = secondPassword;
     }
-    
-    public void validatePasswordConfirmation() {
-        // TODO: check that passwords match
+
+    public boolean isRegisterError() {
+        return registerError;
     }
-    
-    public void validateEmail() {
-        // TODO: 1. Check that email is valid (i.e. regex)
-        // TODO: 2. Check that email is not already taken (i.e. database query)
+
+    public String getErrorMessage() {
+        return errorMessage;
     }
-    
-    public void attemptRegister() {
-        System.out.println("attemptRegister called");
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
-    
+
+    public String attemptRegister() {
+        if (email.isEmpty()) {
+            setRegisterError("Please enter an email.");
+            return "register";
+        }
+        if (!isValidEmail()) {
+            setRegisterError("Not a valid email address.");
+            return "register";
+        }
+        if (firstPassword.isEmpty() || secondPassword.isEmpty()) {
+            setRegisterError("Please enter a password.");
+            return "register";
+        }
+        if (!passwordsMatch()) {
+            setRegisterError("Passwords do not match.");
+            return "register";
+        }
+
+        dbConnector = new DBConnector();
+        final String username = stripEmail(email);
+        System.out.println("username: " + username);
+
+        if (usernameExistsInDatabase(username)) {
+            setRegisterError("Email already exists.");
+            return "register";
+        }
+        errorMessage = "";
+        registerError = false;
+        return "dashboard";
+    }
+
+    private void setRegisterError(String errorMsg) {
+        errorMessage = errorMsg;
+        registerError = true;
+    }
+
+    private boolean isValidEmail() {
+        final Pattern VALID_EMAIL_ADDRESS_REGEX
+                = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        return matcher.find();
+    }
+
+    private boolean passwordsMatch() {
+        return firstPassword.equals(secondPassword);
+    }
+
+    private boolean usernameExistsInDatabase(String username) {
+        return dbConnector.usernameExists(username);
+    }
+
+    private String stripEmail(String email) {
+        // Hacky
+        return email.split("@")[0];
+    }
+
 }
